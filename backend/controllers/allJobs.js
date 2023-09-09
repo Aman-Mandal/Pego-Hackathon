ebfce5f559cc1cca47950aaacbe58a809cd2a4b4
VaibhavArora19 <aroravaibhav817@gmail.com>
2023-09-09 13:17:17 +0530

    Added job handler

---
+++ backend/controllers/allJobs.js
@@ -0,0 +1,55 @@
+import { ethers } from "ethers";
+import { getUserInfo } from "../helpers/polybaseQueries.js";
+import {
+  readTimeBasedJobRecordByAddress,
+  readCustomJobRecordByAddress,
+} from "../helpers/polybaseQueries.js";
+
+export const getAllJobsByUser = async (req, res, next) => {
+  const { userAddress } = req.params;
+
+  try {
+    const formattedUserAddress = ethers.utils.getAddress(userAddress);
+
+    const response = await getUserInfo(formattedUserAddress);
+
+    if (response.data === null) {
+      throw new Error("Something went wrong");
+    }
+
+    res.status(200).json({ success: true, response: response });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getAllDetailedJobs = async (req, res, next) => {
+  const { userAddress } = req.params;
+
+  try {
+    const formattedUserAddress = ethers.utils.getAddress(userAddress);
+
+    const timeBasedJobsData = await readTimeBasedJobRecordByAddress(
+      formattedUserAddress
+    );
+    const timeBasedJobs = timeBasedJobsData?.data.map((job) => {
+      return job?.data;
+    });
+
+    const customJobsData = await readCustomJobRecordByAddress(
+      formattedUserAddress
+    );
+    const customJobs = customJobsData?.data.map((job) => {
+      return job?.data;
+    });
+
+    if (timeBasedJobs === null || customJobs === null) {
+      throw new Error("Something went wrong");
+    }
+    const allJobs = { timeBasedJobs: timeBasedJobs, customJobs: customJobs };
+
+    res.status(200).json({ success: true, response: allJobs });
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ backend/controllers/customJob.js
@@ -0,0 +1,100 @@
+import { ethers } from "ethers";
+import randomstring from "randomstring";
+import {
+  addCustomJobReference,
+  createCustomJobRecord,
+  readCustomBasedJobRecord,
+} from "../helpers/polybaseQueries.js";
+import { createNewUser } from "../helpers/polybaseQueries.js";
+import Account from "../models/Account.js";
+import Job from "../models/Job.js";
+
+export const postCustomJobHandler = async (req, res, next) => {
+  const { name, contractAddress, value, data, scheduledBy, ABI } = req.body;
+
+  try {
+    const formattedContractAddress = ethers.utils.getAddress(contractAddress);
+    const randomId = randomstring.generate() + formattedContractAddress;
+    const formattedScheduledBy = ethers.utils.getAddress(scheduledBy);
+
+    const response = await createCustomJobRecord(
+      randomId,
+      name,
+      formattedContractAddress,
+      ABI,
+      formattedScheduledBy,
+      value,
+      data
+    );
+
+    if (response.data === null) {
+      throw new Error("Creating a custom job failed");
+    }
+
+    const account = await Account.findOne({ address: formattedScheduledBy });
+
+    if (!account) {
+      const response = await createNewUser(formattedScheduledBy);
+      if (response.data === null) {
+        throw new Error("Creating a new user failed!");
+      }
+
+      const newAccount = new Account({
+        address: formattedScheduledBy,
+      });
+
+      await newAccount.save();
+    }
+
+    const userResponse = await addCustomJobReference(
+      formattedScheduledBy,
+      randomId
+    );
+
+    if (userResponse.data === null) {
+      throw new Error("Adding custom based reference failed");
+    }
+
+    const newJob = new Job({
+      jobType: "custom",
+      polybaseId: response.data.id,
+    });
+
+    await newJob.save();
+
+    res
+      .status(201)
+      .json({ success: true, message: "Custom job created successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getCustomJobHandler = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const response = await readCustomBasedJobRecord(id);
+
+    if (response.data === null) {
+      return res
+        .status(404)
+        .json({ success: false, response: "No Job Found!" });
+    }
+
+    res.status(201).json({ success: true, response: response.data });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getCustomJobByUser = async (req, res, next) => {
+  const { userAddress } = req.params;
+  try {
+    const jobs = await readCustomJobRecordByAddress(userAddress);
+
+    res.status(200).json({ success: true, response: jobs });
+  } catch (err) {
+    next(err);
+  }
+};
