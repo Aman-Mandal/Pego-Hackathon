a9ffce7015b67cf398438b70089741da7ab17b43
VaibhavArora19 <aroravaibhav817@gmail.com>
2023-09-09 13:19:12 +0530

    Added schema

---
+++ backend/models/Job.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+
+const jobSchema = new Schema({
+  jobType: {
+    type: String,
+    required: true,
+  },
+  polybaseId: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Job", jobSchema);
