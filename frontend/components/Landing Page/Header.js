a14debbd86b134aece68e190ac41754a3998829d
Aman-Mandal <002amankumar@gmail.com>
2023-09-23 15:53:42 +0530

    Header and Footer for landing page

---
+++ frontend/components/Landing Page/Header.js
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import React from "react";
+import { BsArrowRightShort } from "react-icons/bs";
+import { useRouter } from "next/router";
+
+const Header = () => {
+  const router = useRouter();
+
+  return (
+    <section>
+      <header className="flex mx-auto mt-3 h-[90vh] overflow-hidden ">
+        <div className="flex-[0.4] px-20 flex flex-col justify-center">
+          <h2 className="text-8xl font-Poppins font-medium leading-[105px]">
+            All-in-one <br />
+            automation <br />
+            platform
+          </h2>
+
+          <p className="font-Poppins text-xl font-normal mt-10">
+            Enabling Web3 developers to automate and operate smart contract
+            functions with ease on{" "}
+            <span className="text-[#6736F1] font-semibold">Pego Network</span>.
+          </p>
+
+          <button
+            onClick={() => {
+              router.push("/create");
+            }}
+            className="bg-[#1e1e1e] flex items-center justify-center gap-2 py-3 w-[230px] mt-10 text-white rounded-xl text-2xl"
+          >
+            Launch App{" "}
+            <span>
+              <BsArrowRightShort size={25} />
+            </span>
+          </button>
+        </div>
+
+        <div className="flex-[0.6]  ">
+          <Image
+            className="absolute -right-72 -z-10 top-10 -rotate-[20deg]"
+            src="/svgs/hero.svg"
+            height={1000}
+            width={1000}
+            alt="hero img"
+          />
+        </div>
+      </header>
+    </section>
+  );
+};
+
+export default Header;
---
+++ frontend/components/Layout/Navbar.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { BsArrowRightShort } from "react-icons/bs";
+import { useRouter } from "next/router";
+import { ens } from "../ENS";
+import { useAccount } from "wagmi";
+
+const Navbar = () => {
+  const { address } = useAccount();
+  const router = useRouter();
+  const [ensName, setEnsName] = useState("");
+
+  useEffect(() => {
+    const fetchEns = async () => {
+      const { ensName } = await ens(address);
+      setEnsName(ensName);
+    };
+    fetchEns();
+  }, [address]);
+
+  return (
+    <nav
+      className={`flex justify-between px-20 py-4 border-b border-gray-900 ${
+        router.pathname === "/" ? "bg-white" : "bg-[#18181A]"
+      }`}
+    >
+      <p
+        onClick={() => {
+          router.push("/");
+        }}
+        className={`font-semibold font-Poppins text-2xl cursor-pointer ${
+          router.pathname === "/" ? "text-black" : "text-[#EDEDEF]"
+        }`}
+      >
+        PegoCall.
+      </p>
+      <div className="flex gap-8 items-center font-Poppins">
+        <p
+          onClick={() => {
+            router.push("/");
+          }}
+          className={`${
+            router.pathname === "/" ? "text-black" : "text-[#EDEDEF]"
+          } cursor-pointer`}
+        >
+          Home
+        </p>
+
+        <p
+          onClick={() => {
+            router.push("/create");
+          }}
+          className={`${
+            router.pathname === "/" ? "text-black" : "text-[#EDEDEF]"
+          } cursor-pointer`}
+        >
+          Create Job
+        </p>
+        <button
+          onClick={() => router.push("/dashboard")}
+          className={`flex items-center justify-center font-Poppins gap-2 py-2 px-4 ${
+            router.pathname === "/" ? "bg-black " : "bg-[#443592]"
+          }  rounded-xl text-[#EDEDEF]`}
+        >
+          {router.pathname === "/dashboard" ? (
+            <p>
+              {ensName
+                ? ensName
+                : `${address?.slice(0, 5)}...${address?.slice(
+                    address?.length - 3
+                  )}`}
+            </p>
+          ) : (
+            "Go to Dashboard"
+          )}
+
+          <span>
+            <BsArrowRightShort size={22} />
+          </span>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
